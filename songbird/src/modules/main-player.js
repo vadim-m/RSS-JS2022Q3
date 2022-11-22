345811ee750e62cb2e338014d538033f163bf3cb
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-11-22 23:20:45 +0300

    feat: add player controls logic

--- songbird/src/modules/main-player.js
+++ songbird/src/modules/main-player.js
@@ -1,34 +1,94 @@
 import { addActiveClass, removeActiveClass } from "./utils";
 import { pauseBirdInfoAudio } from "./start-game";
 
-export const gamePlayerPlayBtn = document.querySelector(".player__btn--play");
-export const gamePlayerPlayMute = document.querySelector(
-  ".player__btn--volume"
+const gamePlayerPlayBtn = document.querySelector(".player__btn--play");
+const gamePlayerMuteBtn = document.querySelector(".player__btn--volume");
+const gamePlayerProgress = document.querySelector(".player__progress-range");
+const gamePlayerTimePassed = document.querySelector(".player__time--passed");
+const gamePlayerTimeTotal = document.querySelector(".player__time--total");
+const gamePlayerVolumeProgress = document.querySelector(
+  ".player__volume-range"
 );
 
 const questionAudio = new Audio();
+questionAudio.addEventListener("timeupdate", updateAudioProgress);
+questionAudio.addEventListener("ended", stopCurrentAudio);
 
 gamePlayerPlayBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (questionAudio.paused) {
-    playMusic();
+    playCurrentAudio();
   } else {
-    pauseMusic();
+    pauseCurrentAudio();
   }
 });
 
-questionAudio.addEventListener("ended", (e) => {
-  stopCurrentAudio();
+gamePlayerMuteBtn.addEventListener("click", () => {
+  // ! add func - KASHA
+  if (questionAudio.volume) {
+    addActiveClass(gamePlayerMuteBtn);
+    localStorage.setItem("volume", questionAudio.volume);
+    questionAudio.volume = 0;
+    gamePlayerVolumeProgress.value = 0;
+  } else if (questionAudio.volume === 0) {
+    // ! add func - KASHA
+    const volume = localStorage.getItem("volume");
+    questionAudio.volume = volume ? volume : 100;
+    removeActiveClass(gamePlayerMuteBtn);
+    gamePlayerVolumeProgress.value = volume * 100;
+  }
 });
 
-function playMusic() {
+gamePlayerVolumeProgress.addEventListener("input", () => {
+  const value = gamePlayerVolumeProgress.value;
+
+  if (value == 0) {
+    addActiveClass(gamePlayerMuteBtn);
+  } else {
+    removeActiveClass(gamePlayerMuteBtn);
+    localStorage.setItem("volume", questionAudio.volume);
+  }
+  questionAudio.volume = value / 100;
+});
+
+gamePlayerProgress.addEventListener("input", updatePlayingAudioInfo);
+
+function updateAudioProgress() {
+  const maxValue = gamePlayerProgress.max;
+  const duration = questionAudio.duration;
+  const currentTime = questionAudio.currentTime;
+  const progress = (currentTime / duration) * maxValue;
+  gamePlayerProgress.value = progress ? progress : 0;
+
+  const minutesPassed = Math.floor(currentTime / 60) || "0";
+  const secondsPassed = Math.floor(currentTime % 60) || "0";
+  const minutesDuration = Math.floor(duration / 60) || "0";
+  const secondsDuration = Math.floor(duration % 60) || "0";
+
+  gamePlayerTimePassed.textContent = `${minutesPassed}:${
+    secondsPassed < 10 ? `0${secondsPassed}` : secondsPassed
+  }`;
+  gamePlayerTimeTotal.textContent = `${minutesDuration}:${
+    secondsDuration < 10 ? `0${secondsDuration}` : secondsDuration
+  }`;
+  // gamePlayerTimeTotal =
+}
+
+function updatePlayingAudioInfo() {
+  const maxValue = gamePlayerProgress.max;
+  const progress = gamePlayerProgress.value;
+  console.log(progress);
+  questionAudio.currentTime = (progress / maxValue) * questionAudio.duration;
+}
+
+function playCurrentAudio() {
   pauseBirdInfoAudio();
   questionAudio.play();
   addActiveClass(gamePlayerPlayBtn);
 }
 
-function pauseMusic() {
+function pauseCurrentAudio() {
   questionAudio.pause();
   removeActiveClass(gamePlayerPlayBtn);
 }
