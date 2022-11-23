8cf069e4882b301c33058e580ed09a7f846f2112
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-11-23 04:22:52 +0300

    fix: complete language data array

--- songbird/src/modules/change-interface-language.js
+++ songbird/src/modules/change-interface-language.js
@@ -8,6 +8,26 @@ const textData = {
   game__questions: ["Вопрос:", "Question:"],
   score__text: ["Счёт:", "Score:"],
   footer__info: ["Вадим Монастырский © 2022", "Vadim Monastyrskiy © 2022"],
+  bird__placeholder: [
+    "Прослушайте трек и выберите правильный вариант ответа.",
+    "Listen to the track and choose the correct answer.",
+  ],
+  result__victory: [
+    "Поздравляем! Вы набрали максимум очков!",
+    "Congratulations! You have scored the maximum points!",
+  ],
+  result__message: [
+    "Спасибо за игру. Хотите сыграть ещё раз?",
+    "Thanks for playing. Do you want to play again?",
+  ],
+  "result__btn--game": ["Игра", "Game"],
+  "result__btn--main": ["Главная", "Main"],
+  "result__btn--gallery": ["Галерея", "Gallery"],
+  gallery__btn: ["Главная", "Main"],
+  result__title: ["Результат:", "Result"],
+  "results__btn--game": ["Игра", "Game"],
+  "results__btn--main": ["Главная", "Main"],
+  "results__btn--gallery": ["Галерея", "Gallery"],
 };
 
 function fillElemText(key) {
