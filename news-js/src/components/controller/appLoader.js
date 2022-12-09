6c3581c49f1349c7d7813f73891e04051935b397
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-12-09 19:44:26 +0300

    feat: add my api-key

--- news-js/src/components/controller/appLoader.js
+++ news-js/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'c426a880a96e48bda9f1d9faa27cb188',
         });
     }
 }
