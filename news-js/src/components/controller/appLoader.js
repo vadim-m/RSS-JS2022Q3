da5b44841245071420439fedeeefeb228634a722
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-12-12 19:53:12 +0300

    feat: add typescript to webpack

--- news-js/src/components/controller/appLoader.js
+++ news-js/src/components/controller/appLoader.js
@@ -1,11 +1,12 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: 'c426a880a96e48bda9f1d9faa27cb188',
-        });
-    }
+  constructor() {
+    // ! https://newsapi-redirect-production.up.railway.app/ - DEPLOY PROXY
+    super('https://newsapi.org/v2/', {
+      apiKey: 'c426a880a96e48bda9f1d9faa27cb188',
+    });
+  }
 }
 
 export default AppLoader;
